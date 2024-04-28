755b7c5c1ff24018f4654884c8495e2518d7ad76
fredasu <feisu9@gmail.com>
2024-04-28 03:35:17 -0400

    trying to do enemy chasing but not working

--- Mob.gd
+++ Mob.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+@export var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,6 +8,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	#var direction = (player.global_position() - position).normalized()
+	#var velocity = direction * speed
+	#position += velocity * delta
 	pass
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
