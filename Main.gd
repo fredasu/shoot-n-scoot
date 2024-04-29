c8d9d76fad4e379834133d76a5fa06ab3c045a9c
fredasu <feisu9@gmail.com>
2024-04-28 22:05:42 -0400

    basic shooting: enemy disappears, but not bullet. no moving backwards from recoil

--- Main.gd
+++ Main.gd
@@ -1,76 +1,19 @@
-extends Node
+extends Node2D
 
-@export var mob_scene: PackedScene
-@export var bullet: PackedScene
-var score
+@onready var bullet = preload("res://bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	get_node("icon").look_at(get_global_mouse_position())
 
-
-func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$HUD.show_game_over()
-	$Music.stop()
-	$DeathSound.play()
-
-func new_game():
-	score = 0
-	get_tree().call_group("mobs", "queue_free")
-	$Player.start($StartPosition.position)
-	$StartTimer.start()
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
-	$Music.play()
-
-
-func _on_score_timer_timeout():
-	score += 1
-	$HUD.update_score(score)
-
-
-func _on_start_timer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
-	$HUD.update_score(score)
-
-
-func _on_mob_timer_timeout():
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instantiate()
-
-	# Choose a random location on Path2D.
-	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
-
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
-	# Set the mob's position to a random location.
-	mob.position = mob_spawn_location.position
-
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-
-	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
-
-
-func _on_player_shoot(bullet, position, direction):
-	var spawned_bullet = bullet.instantiate()
-	add_child(spawned_bullet)
-	spawned_bullet.position = position
-	#spawned_bullet._set_direction(direction)
-	
+func _input(event):
+	if event.is_action_released("click"):
+		var bullet1 = bullet.instantiate()
+		bullet1.position = get_node("icon").position
+		bullet1.rotation = get_node("icon").rotation
+		add_child(bullet1)
--- bullet.gd
+++ bullet.gd
@@ -1,21 +1,16 @@
 extends Area2D
-var direction:= Vector2.ZERO
-var speed: int = 100
-var velocity
 
+var speed = 300
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if direction != Vector2.ZERO:
-		velocity = direction.normalized()  * speed
-		global_position += velocity * delta
+func _process(delta):
+	position += transform.x * speed * delta
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()	#deletes self when move offscreen
-	
-func _set_direction(dir):
-	direction = dir
+
+func _on_area_entered(area):
+	print(area.name)
+	area.queue_free()
