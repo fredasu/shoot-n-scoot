8d4edb9c730faddf0ac1e7849553190188dceba5
fredasu <feisu9@gmail.com>
2024-04-29 02:13:40 -0400

    enemy mvt match piazza question

--- Main.gd
+++ Main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var bullet = preload("res://bullet.tscn")
+@onready var enemy = preload("res://enemy.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,3 +16,7 @@ func _input(event):
 		var bullet1 = bullet.instantiate()
 		bullet1.transform = $player/Marker2D.global_transform
 		add_child(bullet1)
+	if event.is_action_released("spawnEnemy"):	#press space bar to make enemy for debugging
+		var enem1 = enemy.instantiate()
+		enem1.start(global_transform, $player)
+		add_child(enem1)
